2036327d40511dca590658c66b57c6d318bab03a
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-12-13 10:26:18 -0800

    Improved code and add decmial abblity to code

--- script.js
+++ script.js
@@ -1,159 +1,195 @@
-const calbuttons = document.querySelectorAll(".calbuton");
-const calbox = document.getElementById("calbox");
-const clearbutton = document.getElementById("c");
-const addbutton = document.getElementById("add");
-let firstNumber = 0;
-let secondNumber = 0;
-let operator = "";
-// let numOfClicks = 0;
-let operatorClicked = false;
 
 
 
+//Variables for the project
+const calbuttons = document.querySelectorAll(".calbuton"); //All the buttons on the calculator
+const calbox = document.getElementById("calbox"); //The display where all the numbers are 
+const clearbutton = document.getElementById("c"); //The button to clear the display
+const addbutton = document.getElementById("add"); //The button to add 
+
+
+
+let firstNumber = 0; //Frist number the users clicks
+let secondNumber = 0; //Second number the users clicks
+//Tells us what operator the users has clicked
+let operator = ""; 
+let operatorClicked = false; 
+
+
+//Updates number on display 
 function updateNumber(number) {
-    if(calbox.innerText === 0){
+    if(calbox.innerText === "0"){
         calbox.innerText = number;
+        
     }
     else{
-        calbox.innerText += number
+        calbox.innerText += number;
+        
     }
     
 
     
 }
 
+//Add function
 function add(number1, number2) {
-    let result =  number1 + number2;
-    console.log("Adding Numbers")
-    console.log("Result " + result)
-    // calbox.innerText = result;
-    return result;
+
+
+    if(number1.includes(".") || number2.includes(".")) { //Checks to make sure the number is a decimal 
+        let decimal1 = parseFloat(number1);
+        let decimal2 = parseFloat(number2);
+
+        return decimal1 + decimal2
+    } else { //if number is not a deccimal will run reqular code
+        let result =  parseFloat(number1) + parseFloat(number2);
+        console.log("Adding Numbers")
+        console.log("Result " + result)
+        return result;
+    }
+    
+   
+
+    
 }
 
 
+//Subtract function
 function subtract(number1, number2) {
-     let result = number1 - number2;
-     console.log("Subtracting Numbers")
-     console.log("N1 " + number1)
-     console.log("N2 " + number2)
-     return result;
- 
+
+
+      if(number1.includes(".") || number2.includes(".")) { //Checks to make sure the number is a decimal 
+        let decimal1 = parseFloat(number1);
+        let decimal2 = parseFloat(number2);
+
+        return decimal1 - decimal2;
+      } else { //if number is not a deccimal will run reqular code
+        let result = number1 - number2;
+        console.log("Subtracting Numbers")
+        console.log("N1 " + number1)
+        console.log("N2 " + number2)
+        console.log("Result " + result)
+        return result;
+      }
 }
 
+//Mutiplication function 
 function mutiplication(number1, number2) {
-     return number1 * number2;
+
+
+    if(number1.includes(".") || number2.includes(".")) { ///Checks to make sure the number is a decimal 
+        let decimal1 = parseFloat(number1);
+        let decimal2 = parseFloat(number2);
+
+        return decimal1 * decimal2;
+    } else { //if number is not a deccimal will run reqular code
+        let result = number1 * number2;
+        console.log("Muitplying Numbers");
+        console.log("N1 " + number1)
+        console.log("N2 " + number2)
+        console.log("Result " + result);
+         return result;
+    }
 }
 
+    
 
 
 
+//Divide function 
 function divide(number1, number2) {
-    if(number2 !== 0) {
-        return number1 / number2;
-    } 
 
+
+
+    if(number1.includes(".") || number2.includes(".")) { //Checks to make sure the number is a decimal 
+        let decimal1 = parseFloat(number1);
+        let decimal2 = parseFloat(number2);
+
+
+        return decimal2 !== 0 ? decimal1 / decimal2 : "ERROR";
+        // return decimal1 / decimal2;
+    } else { //if number is not a deccimal will run reqular code
+        if(number2 !== 0) { //Check to make sure number cant be divided by zero
+            let result = number1 / number2;
+            console.log("N1 " + number1)
+            console.log("N2 " + number2)
+            console.log("Result " + result);
+            return result;
+            
+        } else {
+    //Will return error if number is divided by zero
     return "ERROR";
+        }
+    
+    
+    }
+    
+    
      
 }
 
-// function equal() {
-//    let numtobecalculate = calbox.innerText;
-//    let result;
-//    try {
-//       result = eval(numtobecalculate);
-//       calbox.innerText = result;
-//    } catch {
-//         calbox.innerText = 'ERROR';
-//    }
-// }
 
 
-
+//Clear display 
 function clear() {
    calbox.innerText = '0';
 }
 
 function handelClick(event) {
-    let buttonClicked = event.target;
-  
+    let buttonClicked = event.target; //Make the button clickable and records user response
+     
     
-    // let operator = buttonClicked.innerText;
+ 
     
-    // console.log("DEBUG Second Number " + secondNumber)
-
-    
-   
+   //Update the display 
     if(!isNaN(buttonClicked.innerText) || buttonClicked.innerText === '.') {
          updateNumber(buttonClicked.innerText)
-        //  numOfClicks++;
+        
          return
     }
 
+    //Clear display when user clicks the C button
     else if(buttonClicked.innerText === 'C') {
          clear();
     } 
+    //Declares the operators and helps 
     else if(buttonClicked.innerText === '*' || buttonClicked.innerText === '+' || buttonClicked.innerText === '-' || buttonClicked.innerText === '/'){
         firstNumber = calbox.innerText;
         operator = buttonClicked.innerText;
         calbox.innerText = 0;
     }
+    //After the user uses the equal button
     else {
         secondNumber = calbox.innerText;
 
+        //Addition operatior
         if(operator === "+"){
-            calbox.innerText = add(parseInt(firstNumber), parseInt(secondNumber));
+            // calbox.includes(secondNumber) ? "Is a decmial" : "Not a decimal";
+            calbox.innerText = add(firstNumber, secondNumber);
         }
-    //     numOfClicks = 0;
-    //     firstNumber = calbox.innerText;
-    //     if(!operatorClicked ){
-    //         operator = buttonClicked.innerText;
-    //     } 
-    //     operatorClicked = true
-    //     // calbox.innerText = '0';
-    //     if(buttonClicked.innerText === '=') {
-    //          secondNumber = calbox.innerText;
-    //          console.log("Operator " + operator)
-    //          console.log("pressed equal button")
-    //          if(operator === '+') {
-    //             console.log("Frist Number " + firstNumber)
-    //             console.log("Second Number " + secondNumber)
-    //             calbox.innerText = add(parseInt(firstNumber), parseInt(secondNumber));
-                   
-                   
-    //         }
-             
-    //         if(operator === '-') {
-    //             secondNumber = calbox.innerText;
-    //             console.log("Operator " + operator)
-    //             console.log("pressed subtract button")
-    //             console.log("Second Number " + secondNumber)
-    //             calbox.innerText = subtract(parseInt(firstNumber), parseInt(secondNumber)); 
-    //                   console.log(firstNumber - secondNumber)
-    //         }
 
-    //         if(buttonClicked.innerText === '*') {
-    //             mutiplication(firstNumber, secondNumber);
-    //         }
+        //Subtraction operator
+        if(operator === "-") {
+            secondNumber = calbox.innerText;
+            calbox.innerText = subtract(firstNumber,secondNumber);
+        }
 
-    //         if(buttonClicked.innerText === '/') {
-    //                 divide(firstNumber, secondNumber)
-    //         }
-            
+        //Mutiplcation operator
+        if(operator === "*") {
+            secondNumber = calbox.innerText;
+            calbox.innerText = mutiplication(firstNumber, secondNumber);
+        }
 
-    //     } else {
-    //         firstNumber = calbox.innerText;
-    //         operator = buttonClicked.innerText;
-    //     } 
-
-       
-    // } 
+        //Division operator
+        if(operator === "/") {
+            secondNumber = calbox.innerText;
+            calbox.innerText = divide(firstNumber, secondNumber);
+        }
     
     
  
-
 }
 }
-for(let i = 0; i < calbuttons.length; i++) {
+for(let i = 0; i < calbuttons.length; i++) { //Displays all the calculator buttons and make handel click work
     calbuttons[i].addEventListener("click", handelClick);
    
  }
